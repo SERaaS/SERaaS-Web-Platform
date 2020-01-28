f965ca00e9d3b331fcf7d6788178ce2c4be551af
Andy <aw087ok@gmail.com>
2020-01-28 23:59:31 +0000

    Added login page file to hold placeholder

---
+++ src/components/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoginPage() {
+
+    return (
+        <div className="loginPage">
+            This is my login page.
+        </div>
+    )
+}
+
+export default LoginPage;
