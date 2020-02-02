9a252440283aef71e6c8a995d43706883f51dca4
Andy <aw087ok@gmail.com>
2020-02-02 20:06:14 +0000

    Platform now displays Login Page by default

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// react-bootstrap dependencies
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginPage from './components/LoginPage'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" style={{ marginTop: '40px', marginLeft: '40px', marginRight: '40px' }}>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +29,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <LoginPage />
     </div>
   );
 }
