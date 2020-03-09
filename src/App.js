5e96ec161b5188007614bf85d46a7588de436d2b
Andy <aw087ok@gmail.com>
2020-03-09 19:02:43 +0000

    App now routes appropriately to Login Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <div className="App" style={{ margin: 'auto', width: '75%', marginTop: '20px' }}>
-      <LoginPage />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <LoginPage />
+          </Route>
+        </Switch>
+      </Router>    
     </div>
   );
 };
