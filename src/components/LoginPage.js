4d5cc8a03a0b14a5b504536f567eb25aec207fce
Andy <aw087ok@gmail.com>
2020-03-10 12:53:39 +0000

    Integrated registration and login API endpoints with Login Page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import { Form, Button } from 'semantic-ui-react'
+import APIUtils from '../utilities/APIUtils';
 
 class LoginPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  onChangeUsernameField = (req) => {
+    this.setState({ username: req.target.value });
+  }
+
+  onChangePasswordField = (req) => {
+    this.setState({ password: req.target.value });
+  }
+
+  onClickRegisterButton = async () => {
+    try {
+      const user = await APIUtils.register(this.state.username, this.state.password);
+
+      console.log(user);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  onClickLoginButton = async () => {
+    try {
+      const user = await APIUtils.login(this.state.username, this.state.password);
+
+      console.log(user);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
   render() {
     return (
       <div className="loginPage">
@@ -12,13 +51,14 @@ class LoginPage extends React.Component {
         <Form className='ui segment'>
           <Form.Field>
             <label>Username</label>
-            <input placeholder='Username' type='email' />
+            <input placeholder='Username' onChange={this.onChangeUsernameField} />
           </Form.Field>
           <Form.Field>
             <label>Password</label>
-            <input placeholder='Password' type='password' />
+            <input placeholder='Password' type='password' onChange={this.onChangePasswordField} />
           </Form.Field>
-          <Button type='submit'>Submit</Button>
+          <Button type='submit' color='blue' onClick={this.onClickRegisterButton}>Register</Button>
+          <Button type='submit' color='blue' onClick={this.onClickLoginButton}>Login</Button>
         </Form>
       </div>
     )
