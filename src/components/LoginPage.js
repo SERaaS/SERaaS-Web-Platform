eb98a61fabd1485348541a750d1dbca57bd85f1e
Andy <aw087ok@gmail.com>
2020-03-11 00:39:52 +0000

    Login page now stores to user session handler

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Form, Button } from 'semantic-ui-react'
 import APIUtils from '../utilities/APIUtils';
+import UserSessionHandler from '../utilities/userSessionHandler';
 
 class LoginPage extends React.Component {
 
@@ -10,18 +11,27 @@ class LoginPage extends React.Component {
     // Storing extra metadata to check if credentials are valid
     this.state = {
       username: { text: '', valid: false },
-      password: { text: '', valid: false },
+      password: { text: '', valid: false }
     };
   }
 
+  /**
+   * Checking if the username or password fail generic validation checks.
+   */
   isInvalidCredentials() {
     return !this.state.username.valid || !this.state.password.valid;
   };
 
+  /**
+   * Update the username state upon change.
+   */
   onChangeUsernameField = (req) => {
     this.setState({ username: { text: req.target.value, valid: req.target.value !== '' } });
   };
 
+  /**
+   * Update the password state upon change.
+   */
   onChangePasswordField = (req) => {
     const _password = req.target.value;
     let validPassword = true;
@@ -34,6 +44,9 @@ class LoginPage extends React.Component {
     this.setState({ password: { text: _password, valid: validPassword } });
   };
 
+  /**
+   * Attempt to register user upon button press.
+   */
   onClickRegisterButton = async () => {
 
     if (this.isInvalidCredentials()) {
@@ -42,14 +55,20 @@ class LoginPage extends React.Component {
     };
 
     try {
-      const user = await APIUtils.register(this.state.username, this.state.password);
+      const res = await APIUtils.register(this.state.username.text, this.state.password.text);
 
-      console.log(user);
+      // Set new user as current user and redirect to dashboard
+      UserSessionHandler.setCurrentSession(res.data._id);
     } catch (err) {
       console.log(err);
+
+      alert('Error occurred upon registration attempt.');
     }
   };
 
+  /**
+   * Attempt to login user upon button press.
+   */
   onClickLoginButton = async () => {
 
     if (this.isInvalidCredentials()) {
@@ -58,11 +77,14 @@ class LoginPage extends React.Component {
     };
 
     try {
-      const user = await APIUtils.login(this.state.username, this.state.password);
+      const res = await APIUtils.login(this.state.username.text, this.state.password.text);
 
-      console.log(user);
+      // Set logged user as current user and redirect to dashboard
+      UserSessionHandler.setCurrentSession(res.data._id);
     } catch (err) {
       console.log(err);
+
+      alert('Error occurred upon login attempt.');
     }
   };
   
