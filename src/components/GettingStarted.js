e15774ccf1b152097116f54c8f3188e4ce005308
Andy <aw087ok@gmail.com>
2020-03-28 00:59:13 +0000

    Created documentation component

---
+++ src/components/GettingStarted.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class GettingStarted extends React.Component {
+
+  render() {
+
+    return (
+      <div className="gettingStarted">
+        <div className="ui segment">
+          <h1>Getting Started with SERaaS</h1>
+        </div>
+
+        <div className="ui segment">
+          This is the getting started page of Speech Emotion Recognition as a Service's Web API!
+        </div>
+      </div>
+    );
+  };
+};
+
+export default GettingStarted;
