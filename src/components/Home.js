bf150890e3273552e09d0a9f7830f199465e4a25
Andy <aw087ok@gmail.com>
2020-03-30 17:57:28 +0100

    Removed its own menu from Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,69 +1,14 @@
 import React from 'react';
-import { withRouter } from "react-router-dom";
-import UserSessionHandler from '../utilities/userSessionHandler';
-
-// Render components
-import { Menu } from 'semantic-ui-react'
 
 class Home extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.MENU_ITEMS = {
-      'dashboard': {
-        div: <Menu.Item name='dashboard' onClick={this.onMenuItemClick}>Dashboard</Menu.Item>,
-        route: '/dashboard'
-      },
-    
-      'login': {
-        div: <Menu.Item name='login' onClick={this.onMenuItemClick}>Log In</Menu.Item>,
-        route: '/auth'
-      },
-
-      'getStarted': {
-        div: <Menu.Item name='getStarted' onClick={this.onMenuItemClick}>Getting Started</Menu.Item>,
-        route: '/getstarted'
-      }
-    };
-
-    // Storing whether user is logged in for appropriate tab display
-    this.state = {
-      loggedIn: UserSessionHandler.getCurrentSession() != null
-    };
-  };
-
-  /**
-   * Routing the user to the appropriate component.
-   */
-  onMenuItemClick = (event, { name }) => {
-    
-    if (this.MENU_ITEMS[name]) {
-      this.props.history.push(this.MENU_ITEMS[name].route);
-    }
-  };
-
   render() {
 
-    // Show dashboard or login tab depending on current authenticated status
-    let menuItems = [this.MENU_ITEMS.getStarted.div, this.state.loggedIn ? this.MENU_ITEMS.dashboard.div : this.MENU_ITEMS.login.div];
-
     return (
-      <div className="loginPage">
+      <div className="homePage">
         <div className="ui segment">
           <h1>Home Page</h1>
         </div>
-
-        <Menu>
-          <Menu.Item
-            name='home'
-            onClick={this.onMenuItemClick}
-          >
-            Home
-          </Menu.Item>
-
-          { menuItems }
-        </Menu>
 
         <div className="ui segment">
           This is the home page of Speech Emotion Recognition as a Service's Web Platform!
@@ -73,4 +18,4 @@ class Home extends React.Component {
   };
 };
 
-export default withRouter(Home);
+export default Home;
