859000b542443ab07c5599bc317cfba07563a483
Andy <aw087ok@gmail.com>
2020-04-08 19:07:33 +0100

    Added emotions dropdown list upon API call query

--- src/components/getting-started/MakeSampleAPICall.js
+++ src/components/getting-started/MakeSampleAPICall.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import APIUtils from '../../utilities/APIUtils';
+import { Dropdown } from 'semantic-ui-react'
 
 class MakeSampleAPICall extends React.Component {
 
   constructor(props) {
     super(props);
 
+    this.defaultSelectedEmotion = "all";
+
     this.defaultAPICallInputParameters = {
-      selectedEmotions: [],
+      selectedEmotions: [ this.defaultSelectedEmotion ],
       specifiedPeriod: 0
     };
 
-    this.EMOTIONS_AVAILABLE = [ 'all', 'neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgusted', 'surprised' ];
+    this.EMOTIONS_AVAILABLE = [ 'neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgusted', 'surprised' ];
 
     this.state = {
       // Storing current file being selected for sample API call
@@ -22,7 +25,8 @@ class MakeSampleAPICall extends React.Component {
 
       // User can only make API call once per refresh
       didAPICall: false,
-      APICallOutputBody: null
+      APICallOutputBody: null,
+      selectedEmotionToPlot: this.EMOTIONS_AVAILABLE[0]
     };
   };
 
@@ -63,51 +67,86 @@ class MakeSampleAPICall extends React.Component {
 
     return APIUtils.query(audioFile, this.props.userId, APICallInputParameters.selectedEmotions, specifiedPeriod)
     .then(function(res) {
-      temp.setState({ didAPICall: true, APICallOutputBody: res.data });
+      temp.setState({ didAPICall: true, APICallOutputBody: res.data, selectedEmotionToPlot: APICallInputParameters.selectedEmotions[0] });
     })
     .catch(function(err) {
       temp.setState({ didAPICall: true, APICallOutputBody: { error: err } });
     });
   };
 
+  /**
+   * Sets the currently selected emotion to plot after API call is made.
+   */
+  onEmotionsDropdownChange = (event, { value }) => {
+    this.setState({ selectedEmotionToPlot: value })
+  };
+
+  /**
+   * Generates a list of dropdowns of the emotions available for plotting after API call is made.
+   */
+  getPlotableEmotions = (selectedEmotions) => {
+    const allSelected = selectedEmotions.indexOf("all") >= 0,
+      emotionsMapper = allSelected ? this.EMOTIONS_AVAILABLE : selectedEmotions;
+
+    return emotionsMapper.map(function(emotion) {
+      return { key: emotion, value: emotion, text: emotion };
+    });
+  };
+
   render() {
 
-    const { APICallInputParameters } = this.state;
-    let selectedEmotions = APICallInputParameters.selectedEmotions.length === 0 ? "all" : APICallInputParameters.selectedEmotions.join();
-
-    // TODO: Display results in a graph showcasing the change of emotions overtime
+    const { APICallInputParameters, didAPICall, selectedEmotionToPlot } = this.state,
+      selectedEmotions = APICallInputParameters.selectedEmotions;
+    let selectedEmotionsString = selectedEmotions.length === 0 ? "all" : selectedEmotions.join(),
+      plotableEmotions = this.getPlotableEmotions(selectedEmotions);
 
     return (
       <div>
-        <form onSubmit={this.onFormButtonClick} className="ui form">
+        {/* API Endpoint URL */}  
+        <h3 style={{ border: "dashed", padding: "10px", borderRadius: "10px" }}>POST <span style={{ marginLeft: "30px" }}>TODO_ADD_URL_HERE/analyse/<span className="highlight">{this.props.userId}</span>/<span class="highlight">{selectedEmotionsString}</span>/<span class="highlight">{APICallInputParameters.specifiedPeriod}</span></span></h3>
 
-          {/* API Endpoint URL */}  
-          <h3 style={{ border: "dashed", padding: "10px", borderRadius: "10px" }}>POST <span style={{ marginLeft: "30px" }}>TODO_ADD_URL_HERE/analyse/<span className="highlight">{this.props.userId}</span>/<span class="highlight">{selectedEmotions}</span>/<span class="highlight">{APICallInputParameters.specifiedPeriod}</span></span></h3>
+        {
+          !didAPICall ? 
+        
+          <form onSubmit={this.onFormButtonClick} className="ui form">
+            <br /><div className="fields">
+              <div className="eight wide disabled field">
+                <label>User ID</label>
+                <div className="ui disabled input"><input type="text" placeholder={this.props.userId} /></div>
+              </div>
+            </div>
+            <div className="fields">
+              <div className="eight wide disabled field">
+                <label>Emotions Selected</label>
+                <div className="ui disabled input"><input type="text" placeholder="all" /></div>
+              </div>
+            </div>
+            <div className="fields">
+              <div className="two wide field">
+                <label>Period</label>
+                <div className="ui input"><input type="number" min="0" placeholder="0" onChange={this.onInputPeriodChange} /></div>
+              </div>
+            </div>
 
-          <div className="fields">
-            <div className="eight wide disabled field">
-              <label>User ID</label>
-              <div className="ui disabled input"><input type="text" placeholder={this.props.userId} /></div>
+            <br /><div className="ui input">
+              <input type="file" onChange={this.onInputFileChange} />
             </div>
-          </div>
-          <div className="fields">
-            <div className="eight wide disabled field">
-              <label>Emotions Selected</label>
-              <div className="ui disabled input"><input type="text" placeholder="all" /></div>
-            </div>
-          </div>
-          <div className="fields">
-            <div className="two wide field">
-              <label>Period</label>
-              <div className="ui input"><input type="number" min="0" placeholder="0" onChange={this.onInputPeriodChange} /></div>
-            </div>
-          </div>
+            <br /><br /><button className="ui primary button">Make Sample API Call</button>
+          </form>
 
-          <br /><div className="ui input">
-            <input type="file" onChange={this.onInputFileChange} />
+          :
+
+          <div>
+            <br /><h5>Select Emotion to Plot</h5>
+            <Dropdown
+              placeholder="Plot Emotion"
+              selection
+              options={plotableEmotions}
+              onChange={this.onEmotionsDropdownChange}
+              defaultValue={selectedEmotionToPlot}
+            />
           </div>
-          <br /><br /><button className="ui primary button">Make Sample API Call</button>
-        </form>
+        }
       </div>
     );
   };
