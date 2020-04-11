487cd9b33c3f988afae4d926eb4432825e9358ed
Andy <aw087ok@gmail.com>
2020-04-11 21:37:52 +0100

    Integrated dashboard with new table component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,9 @@ import { withRouter } from "react-router-dom";
 import UserSessionHandler from '../utilities/userSessionHandler';
 import APIUtils from '../utilities/APIUtils';
 
+// UI dedicated components to build up the Dashboard
+import TableAPICallToTimestamps from './dashboard/TableAPICallToTimestamps';
+
 class Dashboard extends React.Component {
 
   constructor(props) {
@@ -15,17 +18,55 @@ class Dashboard extends React.Component {
       this.props.history.push('/auth');
     };
 
+    this.EMOTIONS_AVAILABLE = [ 'neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgusted', 'surprised' ];
+
     this.state = {
       userId: userSession,
 
       // Storing the most recent API call timestamps to visualise them
       APICallTimestamps: [],
-      mostRecentAPICalls: []
+      
+      // Includes individual API call timestamp metadata
+      mostRecentAPICallTimestampData: [],
+
+      // Most recent emotion probabilities queried over time
+      emotionsOverTime: []
     };
   };
 
   /**
-   * Loading in all user's API call timestamp data upon page load.
+   * Generate the emotions' probabilities queried over time, from the
+   * most recent API calls.
+   */
+  generateEmotionProbabilitiesOverTime = (mostRecentAPICallTimestampData) => {
+    let emotionsOverTime = {};
+
+    mostRecentAPICallTimestampData.forEach(function(it) {
+      const uniqueEmotions = {};
+      
+      it.output.forEach(function(_it) {
+        if (!uniqueEmotions[_it.emotion]) {
+          uniqueEmotions[_it.emotion] = _it.probability;
+        };
+      });
+
+      for (let emotion in uniqueEmotions) {
+        if (uniqueEmotions.hasOwnProperty(emotion)) {
+          if (!emotionsOverTime[emotion]) {
+            emotionsOverTime[emotion] = [];
+          }
+
+          emotionsOverTime[emotion].push({ probability: uniqueEmotions[emotion], timestamp: it.dateCreated });
+        };
+      };
+    });
+
+    return emotionsOverTime;
+  };
+
+  /**
+   * Loading in all user's API call timestamp data and generating associated
+   * statistics (i.e. Emotions' probabilities queried over time) upon page load.
    */
   componentDidMount() {
     const temp = this,
@@ -41,25 +82,37 @@ class Dashboard extends React.Component {
       
       // Retrieve the last 10 API calls' timestamp metadata
       let promises = [],
-        _mostRecentAPICalls = [];
+        _mostRecentAPICallTimestampData = [];
       for (let i = startIndex; i < _APICallTimestamps.length; i += 1) {
         promises.push(
           APIUtils.getAPICallTimestampData(temp.state.userId, _APICallTimestamps[i])
           .then(function(_res) {
-            _mostRecentAPICalls.push(_res.data);
+            _mostRecentAPICallTimestampData.push(_res.data);
           })
         )
       };
 
-      // Once all done, update state !
       return Promise.all(promises)
       .then(function() {
-        temp.setState({ APICallTimestamps: _APICallTimestamps, mostRecentAPICalls: _mostRecentAPICalls });
+
+        // Sort by most recent
+        _mostRecentAPICallTimestampData = _mostRecentAPICallTimestampData.sort(function(a, b) { return new Date(b.dateCreated) - new Date(a.dateCreated) });
+
+        // Generate emotions' probabilities queried over time statistic
+        let _emotionsOverTime = temp.generateEmotionProbabilitiesOverTime(_mostRecentAPICallTimestampData);
+
+        // Once all done, update state !
+        temp.setState({ APICallTimestamps: _APICallTimestamps, mostRecentAPICallTimestampData: _mostRecentAPICallTimestampData, emotionsOverTime: _emotionsOverTime });
       });
     });
   };
 
   render() {
+
+    const { mostRecentAPICallTimestampData, emotionsOverTime } = this.state;
+    console.log(mostRecentAPICallTimestampData);
+    console.log(emotionsOverTime);
+
     return (
       <div className='dashboardContent'>
         <div className="ui segment">
@@ -71,7 +124,7 @@ class Dashboard extends React.Component {
             <div className="ui equal width center aligned padded grid">
               <div className="row">
                 <div className="column">
-                  Table of most recent API calls and their timestamps
+                  <TableAPICallToTimestamps mostRecentAPICallTimestampData={mostRecentAPICallTimestampData} />
                 </div>
 
                 <div className="column">
@@ -80,11 +133,11 @@ class Dashboard extends React.Component {
               </div>
               <div className="row">
                 <div className="column">
-                Most recent 10 API calls (selectable via Dropdown) emotion (also selectable via Dropdown) breakdown plots 
+                  Most recent 10 API calls (selectable via Dropdown) emotion (also selectable via Dropdown) breakdown plots 
                 </div>
 
                 <div className="column">
-                Time series plot of Emotion (selectable via Dropdown) queried's probabilities over time (aggregated from most recent 10 API calls)
+                  Time series plot of Emotion (selectable via Dropdown) queried's probabilities over time (aggregated from most recent 10 API calls)
                 </div>
               </div>
             </div>
